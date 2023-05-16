fad971fbd37334ab5aa7dcc1ffa07d3a2bf2a915
200~Deepakpatel001 <deepakpatel970@gmail.com>
2023-05-16 12:50:31 +0530

    added password encryption

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import mysql.connector
 from streamlit_extras.switch_page_button import switch_page
+from cryptography.fernet import Fernet
 
 
 def db_cnx():
@@ -23,15 +24,23 @@ def login():
         password = st.session_state.password
         mydb = db_cnx()
         cnx = mydb.cursor()
-        cnx.execute(f"select * from Admin where User_Name = '{user_name}' and Password = '{password}'")
+        cnx.execute(f"select * from Admin2 where User_Name = '{user_name}'")
         data = cnx.fetchall()
         cnx.close()
         if data:
-            st.success("Logged In")
-            if 'Logged_Username' not in st.session_state:
-                st.session_state['Logged_Username'] = data[0][1]
-                st.session_state['User_Role'] = data[0][4]
-            switch_page("Product")
+            passwd = data[0][2]
+            passwd = passwd.encode()
+            key = b'j8rS20etwOw1qH8XeznLLz_tpmwEUuXhKUs_oS8XgOY='
+            cipher = Fernet(key)
+            passwd = (cipher.decrypt(passwd)).decode()
+            st.success(passwd)
+            if passwd == password:
+                if 'Logged_Username' not in st.session_state:
+                    st.session_state['Logged_Username'] = data[0][1]
+                    st.session_state['User_Role'] = data[0][4]
+                switch_page("Product")
+            else:
+                st.error("Incorrect User name or Password")
         else:
             st.error("Incorrect User name or Password")
 
@@ -47,22 +56,26 @@ def add_customer():
         add_password = st.session_state.add_password
         add_confirm_password = st.session_state.add_confirm_password
         if add_password == add_confirm_password:
-
+            passwd = add_password
+            passwd = passwd.encode()
+            key = b'j8rS20etwOw1qH8XeznLLz_tpmwEUuXhKUs_oS8XgOY='
+            cipher = Fernet(key)
+            passwd = (cipher.encrypt(passwd)).decode()
             mydb = db_cnx()
             cnx = mydb.cursor()
-            sql = f"Select * from admin where user_name = '{add_user_name}'"
+            sql = f"Select * from admin2 where user_name = '{add_user_name}'"
             cnx.execute(sql)
             acc_data = cnx.fetchall()
 
             if acc_data:
                 st.error("User name already exist try another user name")
             else:
-                sql = "INSERT INTO admin(User_Name,Password,Confirm_Password,Role)VALUES (%s, %s, %s, %s)"
-                data1 = (str(add_user_name), str(add_password), str(add_confirm_password), 'Customer')
+                sql = "INSERT INTO admin2 (User_Name,Password,old_Password,Role)VALUES (%s, %s, null , %s)"
+                data1 = (str(add_user_name), str(passwd), 'Customer')
                 cnx.execute(sql, data1)
                 mydb.commit()
                 mydb.close()
-                st.success(f"{add_user_name} Created please Login")
+                st.success("Account Created please Login")
         else:
             st.error("Passwords do not match.")
 
